0d9fcfe037eb6fe352a0cb8e98b2ecc8642cb7ff
ruairikerins <ruairikerins95@gmail.com>
2021-04-28 14:46:31 +0100

    FlaksMega Tutorial blog done up to chaper 3

---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from app import app
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {"username":"Ruairi"}
+    posts =[
+        {
+            "author":{"username":"John"},
+            "body":"Beautiful day in Portland"
+        },
+        {
+            "author": {"username":"Susan"},
+            "body": "The Avengers movie was so cool"
+        }
+    ]
+    return render_template("index.jinja", title="Home", user=user, posts=posts)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Loging requested for user{}, remember_me={}".format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template("login.jinja", title="Sign In", form=form)
